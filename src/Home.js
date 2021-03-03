872ef7a1a76c4dbb6a70a82b33d944c944d6583c
DavorJ94 <davorjovanovic94@gmail.com>
2021-03-03 23:45:51 +0100

    finished homepage

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { initialPrepopulation } from "./actions/initialState";
 import { isFavorite } from "./actions/isFavorite";
 import { willBuy } from "./actions/willBuy";
+import logo from "./images/logo.png";
 
 function HomePage() {
   const dispatch = useDispatch();
@@ -24,12 +25,51 @@ function HomePage() {
     dispatch(isFavorite(e.target.name));
   };
 
+  const checkFavoriteAndWillBuy = () => {
+    let favCounter = 0;
+    let willBuyCounter = 0;
+    images?.map((image) => {
+      if (image.isFavorite) favCounter++;
+      if (image.willBuy) willBuyCounter++;
+    });
+    return { favoritesCount: favCounter, willBuyCount: willBuyCounter };
+  };
+
   const handleWillBuyBtn = (e) => {
     dispatch(willBuy(e.target.name));
   };
 
   return (
     <div>
+      <header className="header">
+        <img className="logo" src={logo} alt=""></img>
+        <div className="fav-cart-container">
+          <button
+            className="headerBtn favoritesBtn"
+            data-favorite-count={checkFavoriteAndWillBuy().favoritesCount}
+          >
+            <i
+              className={
+                checkFavoriteAndWillBuy().favoritesCount !== 0
+                  ? "ri-heart-3-fill"
+                  : "ri-heart-3-line"
+              }
+            ></i>
+          </button>
+          <button
+            className="headerBtn willBuyBtn"
+            data-willBuy-count={checkFavoriteAndWillBuy().willBuyCount}
+          >
+            <i
+              className={
+                checkFavoriteAndWillBuy().willBuyCount !== 0
+                  ? "ri-shopping-cart-2-fill"
+                  : "ri-shopping-cart-2-line"
+              }
+            ></i>
+          </button>
+        </div>
+      </header>
       <div className="grid-content">
         {images?.map((item) => {
           return (
